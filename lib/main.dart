b6bb3df466dd07566970888993dd63dc78a25c00
Lior Breitman <liorbreitman321@gmail.com>
2022-05-22 22:31:56 +0300

    added card

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'front/Supp_front/main_supp.dart';
 import 'front/who_you_are_page.dart';
+import 'front/menu_cards/volunteering_card.dart';
 
-main() => runApp(
-    const MaterialApp(debugShowCheckedModeBanner: false, home: MainSupp()));
+main() => runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: VolunteerCard(
+        activity: "משלוח מזון ", donorName: "לתת", date: "12.12.2001")));
